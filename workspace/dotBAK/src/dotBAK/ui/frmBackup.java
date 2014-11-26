04ba32c6bb5249195c51c05857962bc5f59c938c
ldye <vixxenart@gmail.com>
2014-11-25 22:14:16 -0500

    Added some button functionality

--- workspace/dotBAK/src/dotBAK/ui/frmBackup.java
+++ workspace/dotBAK/src/dotBAK/ui/frmBackup.java
@@ -6,6 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTree;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class frmBackup extends JFrame {
 
@@ -35,8 +45,65 @@ public class frmBackup extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("Settings");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("Archived");
+		
+		JButton btnNewButton_2 = new JButton("Select Files");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		
+		JLabel lblOptions = new JLabel("Options:");
+		
+		JTree tree = new JTree();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(28)
+							.addComponent(lblOptions))
+						.addComponent(btnNewButton_2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGap(36)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(tree, GroupLayout.PREFERRED_SIZE, 229, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(46, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblOptions)
+							.addGap(19)
+							.addComponent(btnNewButton)
+							.addGap(18)
+							.addComponent(btnNewButton_1)
+							.addGap(18)
+							.addComponent(btnNewButton_2))
+						.addComponent(separator, GroupLayout.DEFAULT_SIZE, 232, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(21)
+							.addComponent(tree, GroupLayout.DEFAULT_SIZE, 211, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
--- workspace/dotBAK/src/dotBAK/ui/mainFrame.java
+++ workspace/dotBAK/src/dotBAK/ui/mainFrame.java
@@ -11,6 +11,11 @@ import java.awt.event.ActionEvent;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Color;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
 
 public class mainFrame {
 
@@ -44,27 +49,53 @@ public class mainFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setForeground(new Color(0, 0, 0));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		final dotBAK.ui.frmArchived actArchive = new frmArchived();
+		final dotBAK.ui.frmRestore actRestore = new frmRestore();
+		final dotBAK.ui.frmSettings actSettings = new frmSettings();
+		final dotBAK.ui.frmBackup actBackUp = new frmBackup();
 		JButton btnArchived = new JButton("Archived");
 		btnArchived.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				frame.setVisible(false);
+				actArchive.setVisible(true);
 			}
 		});
 		
 		JButton btnRestore = new JButton("Restore");
 		btnRestore.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				actRestore.setVisible(true);
 			}
 		});
 		
 		JButton btnBackUp = new JButton("Back Up");
+		btnBackUp.addActionListener(new ActionListener(){
+			public void actionPerformed (ActionEvent e){
+				frame.setVisible(false);
+				actBackUp.setVisible(true);
+			}
+		});
+		
+		
 		
 		JButton btnSettings = new JButton("Settings");
+		btnRestore.addActionListener(new ActionListener(){
+			public void actionPerformed (ActionEvent e){
+				frame.setVisible(false);
+				actSettings.setVisible(true);
+			}
+		});
 		
 		JSeparator separator = new JSeparator();
 		separator.setOrientation(SwingConstants.VERTICAL);
+		
+		JLabel lblNewLabel = new JLabel("(dot)BAK ");
+		
+		JLabel lblASuperAwesome = new JLabel("A super awesome backup utility");
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -77,7 +108,14 @@ public class mainFrame {
 						.addComponent(btnArchived, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 13, GroupLayout.PREFERRED_SIZE)
-					.addGap(319))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(64)
+							.addComponent(lblASuperAwesome))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(85)
+							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)))
+					.addGap(74))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -94,7 +132,14 @@ public class mainFrame {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(4)
 					.addComponent(separator, GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(55)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lblASuperAwesome)
+					.addContainerGap(72, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
+		frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{btnSettings, btnBackUp, btnRestore, btnArchived, separator, lblNewLabel}));
 	}
 }
